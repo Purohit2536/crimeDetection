766b2839ec422886fc1b91f4327acee539baf29f
Jemisheth <98176802+Jemisheth@users.noreply.github.com>
2022-03-25 21:29:00 +0530

    added crime marker

--- homepage.java
+++ homepage.java
@@ -2,15 +2,19 @@ package com.example.crimeapp;
 
 import androidx.annotation.NonNull;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,7 +24,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
-
 
 public class HomePage extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,11 +42,11 @@ public class HomePage extends FragmentActivity implements OnMapReadyCallback {
             @Override
             public void onClick(View v) {
 
-                Intent i = new Intent(HomePage.this,EmergencyContacts.class);
+                Intent i = new Intent(HomePage.this, EmergencyContacts.class);
                 startActivity(i);
             }
         });
-        Searchview = findViewById(R.id.Searchview );
+        Searchview = findViewById(R.id.Searchview);
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         assert mapFragment != null;
 
@@ -60,7 +63,7 @@ public class HomePage extends FragmentActivity implements OnMapReadyCallback {
                     e.printStackTrace();
                 }
                 Address address = addressList.get(0);
-                LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
                 return false;
             }
@@ -77,15 +80,39 @@ public class HomePage extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
         LatLng Gujarat = new LatLng(22.463822, 70.079729);
-        map.addMarker(new MarkerOptions().position(Gujarat).title("Darbar Gadh"));
+        map.addMarker(new MarkerOptions().position(Gujarat).title("Murder"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Gujarat));
 
         LatLng Guj = new LatLng(22.463309, 70.058649);
-        map.addMarker(new MarkerOptions().position(Guj).title("Oshwal Colony "));
+        map.addMarker(new MarkerOptions().position(Guj).title("Firing"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Guj));
+
+        LatLng Jamnagar = new LatLng(22.447107, 70.049449);
+        map.addMarker(new MarkerOptions().position(Jamnagar).title("Murder"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Jamnagar));
+
+        LatLng gj10 = new LatLng(22.457235, 70.086542);
+        map.addMarker(new MarkerOptions().position(gj10).title("Vehicle Theft"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(gj10));
+
+        LatLng Lalpur = new LatLng(22.440575, 70.072692);
+        map.addMarker(new MarkerOptions().position(Lalpur).title("Violence"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Lalpur));
 
         googleMap.getUiSettings().setZoomControlsEnabled(true);
         googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
 
     }
 }
