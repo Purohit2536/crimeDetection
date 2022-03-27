07580da3f8d5d8766473e5c232759b4760bb2c62
Jemisheth <98176802+Jemisheth@users.noreply.github.com>
2022-03-27 21:43:06 +0530

    Update Crime_Activity.java

--- Crime_Activity.java
+++ Crime_Activity.java
@@ -1,17 +1,14 @@
 package com.example.crimeapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.net.MalformedURLException;
 
 public class Crime_Activity extends AppCompatActivity {
 
@@ -45,12 +42,63 @@ public class Crime_Activity extends AppCompatActivity {
             }
         });
 
-
         Murderbtn = findViewById(R.id.Murderbtn);
         Murderbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Intent i = new Intent(Crime_Activity.this,MurderActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Murderbtn = findViewById(R.id.Violencebtn);
+        Murderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Crime_Activity.this,ViolenceActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Murderbtn = findViewById(R.id.Firingbtn);
+        Murderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Crime_Activity.this,FiringActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Murderbtn = findViewById(R.id.Bomb_Blast);
+        Murderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Crime_Activity.this,BombBlastActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Murderbtn = findViewById(R.id.Kidnappingbtn);
+        Murderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Crime_Activity.this,KidnappingActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Murderbtn = findViewById(R.id.Burglarybtn);
+        Murderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Crime_Activity.this,BurglaryActivity.class);
+                startActivity(i);
             }
         });
     }
