63192172c2a691c7d25ad5b277e2b06d5ada765d
Jemisheth <98176802+Jemisheth@users.noreply.github.com>
2022-03-30 23:19:24 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,16 @@
+package com.example.crimeapp;
+
+public class User {
+    public String name, email, password, contactNo;
+
+    public User(){
+
+        }
+
+        public User(String name, String email, String password, String contactNo){
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.contactNo = contactNo;
+        }
+}
