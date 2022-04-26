dc66d6aa37becec22e021089bea046167f3f7110
Jemisheth <98176802+Jemisheth@users.noreply.github.com>
2022-04-26 17:09:27 +0530

    map current location

--- ImageActivity.java
+++ ImageActivity.java
@@ -3,8 +3,12 @@ package com.example.crimeapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Binder;
@@ -15,14 +19,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class ImageActivity extends AppCompatActivity {
+public class ImageActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    int SELECT_PHOTO = 1;
+    SupportMapFragment mapFragment;
+    GoogleMap map;
+    int SELECT_PHOTO = 10;
     Uri uri;
     ImageView imageView;
 
@@ -41,13 +50,15 @@ public class ImageActivity extends AppCompatActivity {
             @Override
             public void onNavigationItemReselected(@NonNull MenuItem menuitem) {
 
-                switch (menuitem.getItemId()){
-                    case R.id.crime: startActivity(new Intent(getApplicationContext(),Crime_Activity.class));
-                        overridePendingTransition(0,0);
+                switch (menuitem.getItemId()) {
+                    case R.id.crime:
+                        startActivity(new Intent(getApplicationContext(), Crime_Activity.class));
+                        overridePendingTransition(0, 0);
                         return;
 
-                    case R.id.home: startActivity(new Intent(getApplicationContext(),HomePage.class));
-                        overridePendingTransition(0,0);
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomePage.class));
+                        overridePendingTransition(0, 0);
                         return;
 
                     case R.id.addimage:
@@ -64,7 +75,7 @@ public class ImageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(Intent.ACTION_PICK);
                 i.setType("image/*");
-                startActivityForResult(i,SELECT_PHOTO);
+                startActivityForResult(i, SELECT_PHOTO);
             }
         });
     }
@@ -72,10 +83,10 @@ public class ImageActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null && data.getData() != null ){
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null && data.getData() != null) {
             uri = data.getData();
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                 imageView.setImageBitmap(bitmap);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -84,5 +95,26 @@ public class ImageActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        map = googleMap;
+
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setScrollGesturesEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
     }
 }
